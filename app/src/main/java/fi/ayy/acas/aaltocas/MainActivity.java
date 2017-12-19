b9f8c0ccc0fe0d5720251543a895571e51360cc4
Alison Tshala <tshalaa1@aalto.fi>
2017-12-19 18:34:02 +0200

    First commit

---
+++ app/src/main/java/fi/ayy/acas/aaltocas/MainActivity.java
@@ -0,0 +1,13 @@
+package fi.ayy.acas.aaltocas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
